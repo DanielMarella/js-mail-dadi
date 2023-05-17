bed1a0b9824a028f446a07a9a36cff5b71bedad9
DanielMarella <marella.daniel96@gmail.com>
2023-05-17 15:46:01 +0200

    fix js for better visualization in console who is the computer and who the user

--- js/myscript.js
+++ js/myscript.js
@@ -7,12 +7,12 @@ let  DiceGame = roundNumber(1,6);
 function roundNumber(min, max) {
     let computerNumber = (Math.floor(Math.random() * (max - min + 1) ) + min);
     let userNumber = (Math.floor(Math.random() * (max - min + 1) ) + min);
-    console.log(computerNumber);
-    console.log(userNumber);
+    console.log('computer: ' + computerNumber);
+    console.log('user: ' +userNumber);
     if (computerNumber > userNumber){
         console.log('ha vinto il computer');
     }
     else{
-        console.log("ha vinto l'utente");
+        console.log("Ha vinto l'utente");
     }
 }
