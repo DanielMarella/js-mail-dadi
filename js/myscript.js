efac70ede2a37f26b402aa8285233c371313ad3b
DanielMarella <marella.daniel96@gmail.com>
2023-05-17 15:43:32 +0200

    DiceGame done, visualize only in console

--- js/myscript.js
+++ js/myscript.js
@@ -3,11 +3,16 @@
 
 
 
-
-
-
-let userNumber = Math.floor(Math.random() * 6) + 1;
-console.log(userNumber);
-
-let computerNumber = Math.floor(Math.random() * 6) + 1;
-console.log(userNumber);
+let  DiceGame = roundNumber(1,6);
+function roundNumber(min, max) {
+    let computerNumber = (Math.floor(Math.random() * (max - min + 1) ) + min);
+    let userNumber = (Math.floor(Math.random() * (max - min + 1) ) + min);
+    console.log(computerNumber);
+    console.log(userNumber);
+    if (computerNumber > userNumber){
+        console.log('ha vinto il computer');
+    }
+    else{
+        console.log("ha vinto l'utente");
+    }
+}
