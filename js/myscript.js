83a8f8c18384c179e6f5421f23b5746b5e17da42
DanielMarella <marella.daniel96@gmail.com>
2023-05-17 17:24:46 +0200

    print on the dom the random number and who is the winner with an alert

--- js/myscript.js
+++ js/myscript.js
@@ -1,18 +1,31 @@
 
 
-
+const ulElement = document.querySelector('ul');
 
 
 let  DiceGame = roundNumber(1,6);
 function roundNumber(min, max) {
     let computerNumber = (Math.floor(Math.random() * (max - min + 1) ) + min);
+    document.getElementById('computer').innerHTML = ('computer: ' +computerNumber);
     let userNumber = (Math.floor(Math.random() * (max - min + 1) ) + min);
+    document.getElementById('user').innerHTML = ('user: ' + userNumber);
+
     console.log('computer: ' + computerNumber);
-    console.log('user: ' +userNumber);
-    if (computerNumber > userNumber){
-        console.log('ha vinto il computer');
+    console.log('user: ' + userNumber);
+        if (computerNumber > userNumber){
+            console.log('ha vinto il computer');
+            alert('ha vinto il computer!')
+        }
+        else if (computerNumber < userNumber){
+            console.log("Ha vinto l'utente");
+            alert("Ha vinto l'utente");
+        }
+        else{
+            alert('Pareggio!!!')
+        }
     }
-    else{
-        console.log("Ha vinto l'utente");
-    }
-}
+
+
+
+
+
